3f159bf272cde05a9a3f405721c632bb4487ee14
marjorii <hello@marjorieober.com>
2020-05-30 15:44:41 +0200

    Add second canvas and highlight a pixel

--- src/script/script.js
+++ src/script/script.js
@@ -1,6 +1,8 @@
 const bg = document.getElementById('background');
+const fg = document.getElementById('foreground');
 let ratio =  window.innerHeight / window.innerWidth;
 const ctx = bg.getContext('2d');
+const ctx2 = fg.getContext('2d');
 
 fetch('images/map.jpg')
 .then((response) => response.blob())
@@ -13,5 +15,13 @@ fetch('images/map.jpg')
         bg.width = width;
         bg.height = height;
         ctx.drawImage(img, 0, 0);
+
+        fg.width = width * 4;
+        fg.height = height * 4;
+        ctx2.translate(-0.5, -0.5);
+        ctx2.scale(4, 4);
+        ctx2.lineWidth = 0.25;
+
+        ctx2.strokeRect(33, 12, 1.25, 1.25);
     }
 });
