6305056a18cbd0867c95968f287b618f8ede470d
marjorii <hello@marjorieober.com>
2020-05-30 13:34:14 +0200

    Testing drawing image on canvas

--- src/script/script.js
+++ src/script/script.js
@@ -4,5 +4,17 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-ctx.fillStyle = 'green';
-ctx.fillRect(0, 0, 100, 50);
+fetch('images/path.jpg')
+.then((response) => response.blob())
+.then((blob) => {
+    let img = new Image();
+    img.onload = () => {
+        ctx.drawImage(img, 0, 0);
+    }
+    img.src = URL.createObjectURL(blob);
+    console.log(img);
+})
+
+
+// ctx.fillStyle = 'green';
+// ctx.fillRect(0, 0, 100, 50);
